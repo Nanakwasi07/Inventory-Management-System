10b68e4b5e00b17e346e26527426ec87db4e4188
Nanakwasi07 <lilnanak1z@gmail.com>
2026-02-03 21:58:29 +0000

    Fixed cors

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,6 +1,6 @@
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://cpen-421-inventory-ms.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: ["http://localhost:3000", "https://cpen-421-inventory-ms.vercel.app", "https://inventory-management-system-gamma-six.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 };
